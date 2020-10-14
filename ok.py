d5f34c9fcaba0991faaff8fd472f42218bd507d1
Varona2005 <varonaemiliano0@gmail.com>
2020-10-13 19:28:17 -0500

    lo subi

---
+++ ok.py
@@ -0,0 +1,12 @@
+
+#Banjo
+def areYouPlayingBanjo(name):
+	if name[0] =='r':
+		print (name + 'plays banjo')
+
+	else:
+		print(name + 'does not play banjo')
+	return name	
+
+areYouPlayingBanjo('raul')		
+areYouPlayingBanjo('pepe')
